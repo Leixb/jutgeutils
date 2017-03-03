8993886b62a76ab848ba63fe0f8d3db2efdeba7b
Leix_b <abone9999@gmail.com>
2017-03-03 19:38:53 +0100

    Fixed error when no folder is found

--- add_cases.py
+++ add_cases.py
@@ -1,6 +1,8 @@
 import logging 
 
 import argparse
+from os.path import isdir
+from os import mkdir
 
 import sys
 
@@ -41,6 +43,8 @@ class addcases:
             help='Specify the suffix of the correct output files', default='cor')
         args = parser.parse_args(remaining)
 
+        logging.debug(code)
+
         if args.input_file == sys.stdin: print('Enter input:')
         src_inp = args.input_file.read()
         if args.output_file == sys.stdin: print('Enter output:')
@@ -49,10 +53,13 @@ class addcases:
         output_suffix = args.output_suffix
         input_suffix = args.input_suffix
 
+        dest_folder ='{}/{}'.format(db_folder,code)
+        if not isdir(dest_folder): mkdir(dest_folder) 
+
         if args.number==0:
-            dest = '{}/{}/custom'.format(db_folder,code)
+            dest = '{}/custom'.format(dest_folder)
         else:
-            dest = '{}/{}/custom-{}'.format(db_folder,code,args.number)
+            dest = '{}/custom-{}'.format(dest_folder,args.number)
 
         dest_inp = dest + '.' + input_suffix
         dest_cor = dest + '.' + output_suffix
